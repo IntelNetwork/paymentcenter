dc6c0400ada1d98a9173ce526ceff37b54ed4683
Hank.huang <Hank.huang@shdbr.com>
2020-03-25 18:33:33 +0800

    '增加结算商户接口'

--- paymentcenter-api/src/main/java/org/smartwork/api/TransferApiOrderProvider.java
+++ paymentcenter-api/src/main/java/org/smartwork/api/TransferApiOrderProvider.java
@@ -1,9 +1,11 @@
 package org.smartwork.api;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.google.common.collect.Maps;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.forbes.comm.utils.ConvertUtils;
 import org.forbes.comm.vo.Result;
 import org.smartwork.biz.service.IMchInfoService;
 import org.smartwork.comm.MchStateEnum;
@@ -12,6 +14,7 @@ import org.smartwork.dal.entity.PayChannel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
@@ -47,6 +50,24 @@ public class TransferApiOrderProvider {
             }
         });
         resultMap.setResult(payChannelMap);
+        return resultMap;
+    }
+
+
+    /**获取结算商户
+     * @param channel
+     * @return
+     */
+    @ApiOperation("获取结算商户")
+    @RequestMapping(value = "/channel-mch-info",method = RequestMethod.GET)
+    public Result<Map<String,Object>> channelMchInfo(@RequestParam(value = "channel",required = true)String channel){
+        Result<Map<String,Object>>  resultMap = new Result<>();
+        Map<String,Object> objMap = Maps.newHashMap();
+        MchInfo mchInfo = mhInfoService.getOne(new QueryWrapper<MchInfo>().eq("channel",channel));
+        if(ConvertUtils.isNotEmpty(mchInfo)){
+            objMap.put(mchInfo.getMchId(),mchInfo.getReflectPoints());
+        }
+        resultMap.setResult(objMap);
         return resultMap;
     }
 }
